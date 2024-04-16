a422d5f8546f5bf7d4939dc2c0d24258ba2cbe77
Umesh <81322770+umesh1040@users.noreply.github.com>
2024-04-16 08:45:05 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/api/python", methods=['POST'])
+@app.route("/api/python", methods=['GET'])
 def hello_world():
     return "<p>Hello, World!</p>"
